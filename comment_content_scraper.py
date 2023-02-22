ac476e8c16b634041e5b1ef5d6d20c1c31396973
Crypft <Crypft@students.softuni.bg>
2023-02-22 14:41:22 +0000

    Update
    Update to the content scraper for a different site

--- comment_content_scraper.py
+++ comment_content_scraper.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 # set up the webdriver
 s = Service("path/to/chromedriver")
@@ -11,25 +14,57 @@ options.headless = True
 driver = webdriver.Chrome(service=s, options=options)
 
 # read in the input file with links
-df_links = pd.read_excel('comment_links_blitz.xlsx', header=None, names=['Links'])
-links = df_links['Links'].tolist()
+links = pd.read_excel('comment_links_dnevnik.xlsx', header=None, names=['Links'])
 
 # initialize output list
 output_data = []
 
 # loop through the links and extract the comments
-for link in links:
+for link in links['Links']:
     driver.get(link)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    comments = soup.find_all('div', class_='comment-body')
+
+    # scroll down to the bottom of the page repeatedly until all comments are loaded
+    while True:
+        last_height = driver.execute_script("return document.documentElement.scrollHeight")
+        driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+        time.sleep(2)
+        new_height = driver.execute_script("return document.documentElement.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    comments = soup.find_all('div', class_='cr')
     for comment in comments:
-        comment_text = comment.find('p').get_text()
-        likes = comment.find('i', class_='bx-like').find_next('span', class_='votes').get_text()
-        dislikes = comment.find('i', class_='bx-dislike').find_next('span', class_='votes').get_text()
+        comment_text = ""
+        if comment.find('div', class_='ci'):
+            ci = comment.find('div', class_='ci')
+            if ci.find('div', class_='p') and not ci.find('div', class_='c-reply'):
+                comment_text = ci.find('div', class_='p').get_text(strip=True)
+            else:
+                comment_text = ci.get_text(strip=True)
+        likes = 0
+        if comment.find('a', class_='e-plus'):
+            likes = int(comment.find('a', class_='e-plus').get_text(strip=True))
+        dislikes = 0
+        if comment.find('a', class_='e-minus'):
+            dislikes = int(comment.find('a', class_='e-minus').get_text(strip=True))
         output_data.append([link, comment_text, likes, dislikes])
 
 # write the output to a new excel file
 output_df = pd.DataFrame(output_data, columns=['Link', 'Comment', 'Likes', 'Dislikes'])
+
+# merge duplicate link cells
+output_df['Link'] = output_df['Link'].fillna(method='ffill')
+
+# remove duplicate rows
+output_df.drop_duplicates(subset=['Link', 'Comment', 'Likes', 'Dislikes'], inplace=True)
+
+# fill in empty link cells
+output_df['Link'] = output_df['Link'].fillna('')
+
+# write the output to a new Excel file
 output_df.to_excel('output_file.xlsx', index=False)
 
 # quit the webdriver
--- comment_scraper.py
+++ comment_scraper.py
@@ -1,9 +1,38 @@
+# import requests
+# from bs4 import BeautifulSoup
+# import pandas as pd
+#
+# # Read input Excel file
+# df = pd.read_excel('article_links.xlsx', header=None, names=['Link'])
+#
+# # Loop through links and extract comment numbers
+# comment_counts = []
+# for link in df['Link']:
+#     response = requests.get(link)
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#         comment_count = soup.select_one('a.comments > span')
+#         if comment_count:
+#             comment_counts.append(int(comment_count.text.strip()))
+#         else:
+#             comment_counts.append(0)
+#     else:
+#         comment_counts.append(None)
+#
+# # Add comment numbers to DataFrame
+# df['Comment Count'] = comment_counts
+#
+# # Write output to new Excel file
+# df.to_excel('output_comment_counts.xlsx', index=False)
+#
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 # Read input Excel file
-df = pd.read_excel('article_links.xlsx', header=None, names=['Link'])
+df = pd.read_excel('article_links_blitz.xlsx', header=None, names=['Link'])
 
 # Loop through links and extract comment numbers
 comment_counts = []
@@ -11,9 +40,14 @@ for link in df['Link']:
     response = requests.get(link)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        comment_count = soup.select_one('a.comments > span')
+        comment_count = soup.select_one('span[itemprop="http://schema.org/interactionCount"] > a')
         if comment_count:
-            comment_counts.append(int(comment_count.text.strip()))
+            comment_count_text = comment_count.text.strip()
+            comment_count_num = re.findall(r'\d+', comment_count_text)
+            if comment_count_num:
+                comment_counts.append(int(comment_count_num[0]))
+            else:
+                comment_counts.append(0)
         else:
             comment_counts.append(0)
     else:
@@ -24,4 +58,3 @@ df['Comment Count'] = comment_counts
 
 # Write output to new Excel file
 df.to_excel('output_comment_counts.xlsx', index=False)
-
